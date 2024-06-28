f6c3e60c6d8b04cd70fceeda618734d40105dfbf
uyarci hilal <hilal.uyarci@gmail.com>
2024-06-28 10:05:07 +0200

    corriger

---
+++ .history/src/components/header/index_20240627142504.jsx
@@ -0,0 +1,37 @@
+import React from 'react'
+import styled from 'styled-components'
+import logo from '../../assets/logo.png'
+import colors from '../../utils/style/colors'
+import Navigation from '../nav'
+
+const HeaderWrapper = styled.header`
+  display: flex;
+  flex
+  justify-content: space-between;
+  align-items: center;
+  position: relative;
+  padding: 3rem 6rem;
+  height: 68px;
+  color: ${colors.primary};
+  font-weight: 700;
+  @media (max-width: 768px) {
+    padding: 3rem 2rem;
+  }
+`
+
+const Logo = styled.img`
+  height: 50px;
+  @media (max-width: 768px) {
+    height: 100px;
+    width: 200px;
+  }
+`
+function Header() {
+  return (
+    <HeaderWrapper>
+      <Logo src={logo} alt="Logo du header" />
+      <Navigation />
+    </HeaderWrapper>
+  )
+}
+export default Header
---
+++ .history/src/pages/home/index_20240627102724.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import styled from 'styled-components'
+import Banner from '../../components/banner'
+import colors from '../../utils/style/colors'
+import Card from '../../components/card'
+
+const Body = styled.div`
+  background-color: ${colors.colorTertiary};
+  padding: 0 6rem;
+  margin-bottom: 20px;
+  @media (max-width: 768px) {
+    padding: 0 3px;
+  }
+`
+
+function Home() {
+  return (
+    <Body>
+      <Banner />
+      <Card />
+    </Body>
+  )
+}
+
+export default Home
