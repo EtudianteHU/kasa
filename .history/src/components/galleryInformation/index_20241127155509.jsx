e8ce541125ee717ee2e2c6c0e4fe087f5704f3af
uyarci hilal <hilal.uyarci@gmail.com>
2024-11-27 18:46:17 +0100

    mettre a jour

---
+++ .history/src/components/galleryInformation/index_20241127155509.jsx
@@ -0,0 +1,103 @@
+import React from 'react'
+import styled from 'styled-components'
+import TagInformation from '../tag/index'
+import Host from '../host/index'
+import RatingInformation from '../rating/index'
+import data from '../../components/data/logements.json'
+import { useParams } from 'react-router-dom'
+import colors from '../../colors'
+
+const WrapperContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  &:first-child {
+    margin-left: 0;
+  }
+
+  &:last-child {
+    margin-right: 0;
+  }
+  width: 100%;
+  @media screen and (width<=768px) {
+    flex-direction: column;
+    gap: 0;
+  }
+`
+
+const Title = styled.div`
+  display: flex;
+  font-size: 35px;
+  color: ${colors.primary};
+  font-weight: 500;
+  margin-top: 1rem;
+  margin-bottom: 0;
+  @media screen and (max-width: 768px) {
+    font-size: 20px;
+  }
+     @media screen and (min-width: 768px) and (max-width: 1024px) {
+  
+`
+
+const SectionLeft = styled.div`
+  display: flex;
+  flex-direction: column;
+  @media screen and (max-width: 768px) {
+    width: 100%;
+  }
+`
+const SectionRight = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-end;
+  @media (max-width: 768px) {
+    padding: 0 2rem;
+    flex-direction: row;
+    align-items: flex-start;
+    width: 100%;
+  }
+
+   {
+    flex-wrap: wrap-reverse;
+  }
+`
+const Location = styled.div`
+  font-weight: 400;
+  font-size: 18px;
+  line-height: 26px;
+  color: ${colors.primary};
+  margin-bottom: 0.2em;
+  @media screen and (width<=768px) {
+    font-size: 14px;
+  }
+`
+
+const WrapperTitle = styled.div`
+  display: flex;
+  flex-direction: column;
+`
+
+const Gallery = () => {
+  const { galleryId } = useParams()
+  const logement = data.find((product) => product.id === galleryId)
+
+  const { title, location, tags, host } = logement
+
+  return (
+    <WrapperContainer>
+      <SectionLeft>
+        <WrapperTitle>
+          <Title>{title}</Title>
+          <Location>{location}</Location>
+        </WrapperTitle>
+        <TagInformation tag={tags} />
+      </SectionLeft>
+      <SectionRight>
+        <Host host={host} />
+        <RatingInformation />
+      </SectionRight>
+    </WrapperContainer>
+  )
+}
+
+export default Gallery
---
+++ .history/src/pages/about/index_20241127101000.jsx
@@ -0,0 +1,84 @@
+// src/components/about/index.js
+import React from 'react'
+import styled from 'styled-components'
+import Collapse from '../../components/collapse' // Le composant Collapse commun
+import aboutData from '../../components/data/aproposData'
+import aboutImg from '../../assets/aproposimg.png'
+import colors
+
+const Section = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: calc(100%-30px);
+  padding: 3rem 6rem;
+  @media screen and (max-width: 768px) {
+    padding: 15px;
+  }
+`
+
+const TextContent = styled.p`
+  font-weight: 400;
+  font-size: 25px;
+  color: black;
+  margin: 30px 20px;
+  width: 100%;
+  @media screen and (max-width: 768px) {
+    margin: auto;
+    font-size: 14px;
+  }
+  @media screen and (min-width: 768px) and (max-width: 1024px) {
+    margin: 0px;
+    color: ${colors.primary};
+  }
+`
+
+const AboutImage = styled.div`
+  width: 100%;
+  height: 220px;
+  background-image: linear-gradient(
+      to bottom,
+      rgba(0, 0, 0, 0.3),
+      rgba(0, 0, 0, 0.5)
+    ),
+    url(${aboutImg});
+  background-size: cover;
+  background-position: center;
+  border-radius: 25px;
+  margin-bottom: 25px;
+
+  back-ground-color: rgb(255, 96, 96);
+  @media screen and (max-width: 768px) {
+    width: 100%;
+    height: 180px;
+  }
+`
+
+const CollapseWrapper = styled.div`
+  width: 100%;
+  margin-bottom: 20px;
+`
+
+const contentMap = {
+  Fiable: aboutData.fiable,
+  Respect: aboutData.respect,
+  Service: aboutData.service,
+  Securité: aboutData.securite,
+}
+
+const About = () => {
+  return (
+    <Section>
+      <AboutImage />
+      {['Fiable', 'Respect', 'Service', 'Securité'].map((title, index) => (
+        <CollapseWrapper key={index}>
+          <Collapse title={title}>
+            <TextContent>{contentMap[title]}</TextContent>
+          </Collapse>
+        </CollapseWrapper>
+      ))}
+    </Section>
+  )
+}
+
+export default About
